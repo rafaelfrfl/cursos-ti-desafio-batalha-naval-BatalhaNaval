5d27a4edb871e7506ad09bbc76cfbb037ba722cd
rafaelfrfl <rafael2017fernando@gmail.com>
2025-02-10 09:16:47 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,8 +4,11 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
+int main([4]<[6]==[4]>[6]) {
+printf(tabuleiro vertical)
+prinft(tabuleiro horizonta)
+
+
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
